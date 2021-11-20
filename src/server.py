06234e7846f3c2727b89edc7acad707c99d405c7
andrewzeitler <andrew.zeitler@faire.com>
2021-11-20 16:16:43 -0500

    Added some server code

--- src/server.py
+++ src/server.py
@@ -1,11 +1,24 @@
+from socket import *
+import ssl
 
 class Server:
-    def __init__(self):
+    def __init__(self, peer_address, peer_port, password):
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        context.verify_mode = ssl.CERT_REQUIRED
+        context.check_hostname = True
+        context.load_default_certs()
+
+        sock = socket.socket(AF_INET, SOCK_STREAM)
+        sock.bind((peer_address, peer_port))
+        sock.listen()
+    
+        with context.wrap_socket(sock, server_side=True) as ssock:
+            conn, addr = ssock.accept()
+            self.connection = conn
+
+    def listen_for_file(self):
         pass
-
-
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
+    
+    def send_file(self):
+        pass
+        
